d90be3e6cd80736d28df7586cae68d8aefbf4e26
Andy Malak <ajm2049@rit.edu>
2023-03-18 23:08:14 -0400

    Fixed checkUser function

--- checkUser.py
+++ checkUser.py
@@ -4,14 +4,8 @@ from datetime import datetime
 
 
 def checkUser(curs, username):
-    curs.execute("SELECT username from User;")
-    usernames = curs.fetchall()
-    for name in usernames:
-        if name == username:
-            return True
-        else:
-            pass
-    return False
+    curs.execute("SELECT * from \"User\" WHERE username = '%s';"% username)
+    return(curs.fetchall() != [])
 
 def createUser(curs, username):
     password = str(input("What would you like your password to be? "))
@@ -20,7 +14,7 @@ def createUser(curs, username):
     email = str(input("What is a good email? "))
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-    createUserQuery = "insert into User values('1000','" + dt_string + "','" + username + "','" + password \
+    createUserQuery = "insert into \"User\" values('1000','" + dt_string + "','" + username + "','" + password \
                       + "','" + fname + "','" + lname + "','" + email + "');"
     print(createUserQuery)
     curs.execute(createUserQuery)
