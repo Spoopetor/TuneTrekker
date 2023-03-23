4afd049d99eb314a86dce8448588371b09b0101d
jsh2915 <jsh2915@rit.edu>
2023-03-23 16:48:32 -0400

    GOt somethiong goign baby. cookin 100

--- randomizer.py
+++ randomizer.py
@@ -1,12 +1,18 @@
 import random
 
+import model
+
+
 def randomSongArtist():
     artist = []
     song = []
-    i=1
-    while(i<=500):
+    i = 1
+    while(i <= 500):
         artist.append(i)
         song.append(i)
         i += 1
     random.shuffle(song)
+    for x in range(500):
+        model.dbExecute("insert into \"SongArtist\" (sid, artistid) values ({}, {});".format(song[x], artist[x]))
 
+randomSongArtist()
