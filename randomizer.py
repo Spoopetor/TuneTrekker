32e903b8c75071a4b9e125ec98b6b9010b279083
jsh2915 <jsh2915@rit.edu>
2023-03-23 20:44:52 -0400

    Finished up the randomizer for Ratings and Listens

--- randomizer.py
+++ randomizer.py
@@ -1,5 +1,5 @@
 import random
-
+from datetime import datetime
 import model
 
 
@@ -14,6 +14,7 @@ def randomSongArtist():
     random.shuffle(song)
     for x in range(500):
         model.dbExecute("insert into \"SongArtist\" (sid, artistid) values ({}, {});".format(song[x], artist[x]))
+        print("success")
 
 def randomSongAlbum():
     album = []
@@ -26,20 +27,37 @@ def randomSongAlbum():
     random.shuffle(song)
     for x in range(500):
         model.dbExecute("insert into \"SongAlbum\" (sid, albumid, trackid) values ({}, {}, {});".format(song[x], album[x], 1))
+        print("success")
 
 def randomFollowing():
     follower = []
     following = []
-    for i in range(1,250):
+    for i in range(1,251):
         follower.append(i)
-        following.append(i+249)
+        following.append(i+250)
         i += 1
     random.shuffle(following)
     for j in range(250):
         if j % 3 == 0:
-            model.dbExecute("insert into \"follows\" (follower, following) values ({}, {});".format(follower[j], following[j]))
+            model.dbExecute("insert into \"Follows\" (follower, following) values ({}, {});".format(follower[j], following[j]))
+            print("success")
         else:
-            model.dbExecute("insert into \"follows\" (follower, following) values ({}, {});".format(follower[j], following[j]))
-            model.dbExecute("insert into \"follows\" (follower, following) values ({}, {});".format(following[j], follower[j]))
+            model.dbExecute("insert into \"Follows\" (follower, following) values ({}, {});".format(follower[j], following[j]))
+            print("success")
+            model.dbExecute("insert into \"Follows\" (follower, following) values ({}, {});".format(following[j], follower[j]))
+            print("success")
 
-randomSongAlbum()
+
+def randomRatingListens():
+    for x in range(1,501):
+        listencount = random.randint(0,10000)
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+        rating = random.randint(1,5)
+        sid = random.randint(1,500)
+        model.dbExecute("insert into \"Listens\" (uid, sid, lastlistened, listencount) values ({}, {}, '{}', {});".format(x, sid, dt_string, listencount))
+        model.dbExecute("insert into \"Rating\" (uid, sid, rating) values ({}, {}, {});".format(x, sid, rating))
+        model.dbExecute("update \"Song\" set listencount = listencount + {} where sid = {};".format(listencount, sid))
+        print("success")
+
+randomRatingListens()
